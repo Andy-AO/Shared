4652477ac1463b00e9d90edd4ccdac90a8dfa017
AndyAO <Zen96285@gmail.com>
2022-08-09 10:04:33 +0800

    exit_code_500

---
+++ exit_code_500/App1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+var app2 = new Process()
+{
+    StartInfo =
+    {
+        FileName = "App2.exe",
+        UseShellExecute = false,
+        CreateNoWindow = false
+    }
+};
+app2.Start();
+
+
---
+++ exit_code_500/App2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("App2 is running.");
+while (true)
+{
+    string input = Console.ReadLine();
+    Console.WriteLine("Input is " + input);
+}
